48e3cf6aaa8b1611d987fbb30f23354741548101
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-03-28 17:33:00 -0300

    Adicionando anotações na classe de autenticação de login

--- src/main/java/com/crudFrontend/crud/Controller/AuthenticationController.java
+++ src/main/java/com/crudFrontend/crud/Controller/AuthenticationController.java
@@ -10,16 +10,21 @@ import com.crudFrontend.crud.Service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
+// a anotação restcontroller define a classe como um controller
 @RestController
+// a anotação requestmapping é responsável por definir a rota de acesso aos metodos da classe
 @RequestMapping("/login")
 public class AuthenticationController {
-    
+    // a anotação autowired é responsavel por ?
     @Autowired
     private AuthenticationService authenticationService;
 
+    // @postmapping vai garantir que para a rota definida, o metodo retornado será do tipo POST
     @PostMapping("/autenticar")
     public String login(@RequestBody LoginRequest Loginrequest){
         boolean autenticado = authenticationService.autenticar(Loginrequest.getCpf(), Loginrequest.getNome());
+        // "?" abaixo se trata de um operador ternário e ele é responsável por decidir qual resposta retornar com base no valor booleano
+        // se o valor em questão for 'True', retornará "login bem sucedido" caso contrário 'False' retornará "Usuário nao encontrado!"
         return autenticado ? "Login bem-sucedido!" : "Usuário não encontrado!";
     }
 }
