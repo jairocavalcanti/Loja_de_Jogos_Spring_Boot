4d6ef3ba6524f1b36058cee36f1ea60ac83486da
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-06-07 19:57:57 -0300

    Adicionando entidade modelo para carrinho e itens do carrinho

---
+++ src/main/java/com/crudFrontend/crud/Model/Carrinho.java
@@ -0,0 +1,35 @@
+package com.crudFrontend.crud.Model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Carrinho {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "carrinho", cascade = CascadeType.ALL)
+    private List<ItemCarrinho> itens = new ArrayList<>();
+
+    @OneToOne
+    private Pessoa usuario;
+
+    public BigDecimal getTotal(){
+        return itens.stream()
+        .map(item -> item.getJogo().getPreco().multiply(BigDecimal.valueOf(item.getQuantidade())))
+        .reduce(BigDecimal.ZERO, BigDecimal::add);
+        
+    }
+    
+}
---
+++ src/main/java/com/crudFrontend/crud/Model/ItemCarrinho.java
@@ -0,0 +1,57 @@
+package com.crudFrontend.crud.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ItemCarrinho {
+
+    @Id 
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Jogo jogo;
+
+    private int quantidade;
+
+    @ManyToOne
+    private Carrinho carrinho;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Carrinho getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(Carrinho carrinho) {
+        this.carrinho = carrinho;
+    }
+
+
+}
