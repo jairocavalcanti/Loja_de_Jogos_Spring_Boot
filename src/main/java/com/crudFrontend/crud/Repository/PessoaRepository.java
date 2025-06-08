af6a169eb4f950946fd5094e948836720c746c38
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-06-08 16:27:53 -0300

    Adicionando anotações na interface repository destinada a entidade pessoa

--- src/main/java/com/crudFrontend/crud/Repository/PessoaRepository.java
+++ src/main/java/com/crudFrontend/crud/Repository/PessoaRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.crudFrontend.crud.Model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+    // o metodo abaixo executa a query automaticamente através do JpaRepository
+    // o padrão de nome "findby" é determinante para que a consulta ocorra de forma correta 
+    /*SELECT * FROM pessoa WHERE cpf = ? AND nome = ?; */
     Optional<Pessoa> findByCpfAndNome(String cpf, String nome);
     Optional<Pessoa> findByCpf(String cpf);
 }
