079844fe2e307a007ac201752f6fce3a00c466ba
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-06-11 02:02:00 -0300

    Inserindo classes service (lógica) para metodos de atualização e deleção de jogos

--- src/main/java/com/crudFrontend/crud/Service/JogoService.java
+++ src/main/java/com/crudFrontend/crud/Service/JogoService.java
@@ -26,4 +26,18 @@ public class JogoService {
     public Jogo savejogo(Jogo jogo) {
         return jogoRepository.save(jogo);
     }
+
+    public Jogo updateJogo(Long id, Jogo jogoAtualizado){
+        Jogo jogo = jogoRepository.findById(id).
+             orElseThrow(() -> new RuntimeException("jogo não encontrado!"));
+        jogo.setNome(jogoAtualizado.getNome());
+        jogo.setDescrição(jogoAtualizado.getDescricao());
+        jogo.setPreco(jogoAtualizado.getPreco());
+        return jogoRepository.save(jogoAtualizado);
+    }
+
+    public void deleteJogo(Long id){
+        jogoRepository.deleteById(id);
+    }
+
 }
