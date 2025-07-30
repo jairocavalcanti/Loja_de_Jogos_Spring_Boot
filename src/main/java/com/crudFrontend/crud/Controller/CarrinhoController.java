ac4eb3d49b5baa768168d7d4745aa2c280d48ae4
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-07-30 03:00:05 -0300

    adicionando rotas para aprimoramentos e novas funcionalidades

--- src/main/java/com/crudFrontend/crud/Controller/CarrinhoController.java
+++ src/main/java/com/crudFrontend/crud/Controller/CarrinhoController.java
@@ -3,14 +3,15 @@ package com.crudFrontend.crud.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudFrontend.crud.DTO.CarrinhoComNomeDTO;
-import com.crudFrontend.crud.DTO.CarrinhoComPessoaDTO;
 import com.crudFrontend.crud.Service.CarrinhoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/carrinho")
@@ -19,16 +20,25 @@ public class CarrinhoController {
     @Autowired
     private CarrinhoService carrinhoservice;
 
-    @PostMapping("/{cpf}")
-    public ResponseEntity<CarrinhoComPessoaDTO> createCarrinho(@PathVariable String cpf) {
-        CarrinhoComPessoaDTO carrinho = carrinhoservice.buscarOuCriarCarrinho(cpf);
+    @GetMapping("/{cpf}")
+    public ResponseEntity<CarrinhoComNomeDTO> visualizarCarrinho(@PathVariable String cpf) {
+        CarrinhoComNomeDTO carrinho = carrinhoservice.buscarCarrinho(cpf);
         return ResponseEntity.ok(carrinho);
     }
 
+    /// metodo criarCarrinho alterado para retornar apenas mensagem de criação ou não
     @PostMapping("/postcarrinho/{cpf}")
-    public ResponseEntity<CarrinhoComNomeDTO> createCarrinhoComNome(@PathVariable String cpf) {
-        CarrinhoComNomeDTO carrinho = carrinhoservice.buscarOuCriarCarrinhoNome(cpf);
+    public ResponseEntity<String> CriarCarrinho(@PathVariable String cpf) {
+        String carrinho = carrinhoservice.CriarCarrinho(cpf);
         return ResponseEntity.ok(carrinho);
+    }
+
+    @PostMapping("/{cpf}/adicionar") 
+    public ResponseEntity<Void> adicionarItem(@PathVariable String cpf,
+            @RequestParam Long idJogo,
+            @RequestParam int quantidade) {
+        carrinhoservice.adicionarAoCarrinho(cpf, idJogo, quantidade);
+        return ResponseEntity.ok().build();
     }
 
 }
