ccedef0e69190d90237cb50130b217ae053edb84
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-07-30 02:56:46 -0300

    adicionando novo DTO para o carrinho com dados do usuário

--- src/main/java/com/crudFrontend/crud/DTO/CarrinhoComNomeDTO.java
+++ src/main/java/com/crudFrontend/crud/DTO/CarrinhoComNomeDTO.java
@@ -1,46 +1,8 @@
 package com.crudFrontend.crud.DTO;
 
-import com.crudFrontend.crud.Model.Carrinho;
+import java.math.BigDecimal;
 
-public class CarrinhoComNomeDTO {
-    
-    private String nome;
-    private String cpf;
-    private Carrinho carrinho;
+import java.util.List;
 
-    
-    public CarrinhoComNomeDTO(){
-
-    }
-
-    public CarrinhoComNomeDTO(String nome, Carrinho carrinho,String cpf) {
-        this.nome = nome;
-        this.carrinho = carrinho;
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Carrinho getCarrinho() {
-        return carrinho;
-    }
-
-    public void setCarrinho(Carrinho carrinho) {
-        this.carrinho = carrinho;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
+public record CarrinhoComNomeDTO(String nomePessoa, String cpf, BigDecimal total, List<ItemCarrinhoDTO> itens) {
 }
