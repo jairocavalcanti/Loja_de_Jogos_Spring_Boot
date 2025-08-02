f8f0804226d0c2f0553a846c4a1eb3c98db8c630
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-08-02 16:39:09 -0300

    Melhorando comentários para melhor entendimento

--- src/main/java/com/crudFrontend/crud/Service/CarrinhoService.java
+++ src/main/java/com/crudFrontend/crud/Service/CarrinhoService.java
@@ -43,11 +43,10 @@ public class CarrinhoService {
     for (ItemCarrinho item : carrinho.getItens()) {
       // novo objeto jogo armazenando jogo presente na lista de itens
       Jogo jogo = item.getJogo();
-      // lista de DTO de itens adicionando novo objeto dto na lista tipo ItemCarrinho
-      // com atributos
-      // de objeto jogo para visualização
-      // abaixo estamos adicionando um DTO do objeto jogo dentro de uma lista do tipo
-      // item carrinho DTO
+      /*
+        lista itensDTO adicionando novo objeto dto tipo ItemCarrinho com atributos de objeto jogo para visualização.
+        abaixo estamos adicionando um novo DTO do objeto jogo (ItemCarrinhoDTO) na lista itens.DTO
+      */
       itensDTO.add(new ItemCarrinhoDTO(
           jogo.getId(),
           jogo.getNome(),
@@ -56,11 +55,11 @@ public class CarrinhoService {
           jogo.getPreco()));
     }
 
-    // finalmente retornando um novo DTO que possui a lista dos DTOS de jogo feita
-    // anteriormente (linha acima)
-    // Esse DTO serve para visualização tanto do usuario que adicionou o item no
-    // carrinho quanto que para a visualização
-    // do total (valor) e dos itens adicionados
+    /*
+      finalmente retornando um novo DTO que possui a lista dos DTOS de jogo feita anteriormente (linha acima)
+      Esse DTO serve para visualização tanto do usuario que adicionou o item no
+      carrinho quanto que para a visualização do total (valor) e dos itens adicionados que foram populados na lista acima (ItensDTO)
+    */
     return new CarrinhoComNomeDTO(
         pessoa.getNome(),
         pessoa.getCpf(),
@@ -80,53 +79,48 @@ public class CarrinhoService {
       carrinhoRepository.save(new Carrinho(pessoa));
     }
     /*
-     * Carrinho carrinho = carrinhoRepository.findByPessoa(pessoa)
-     * .orElseGet(() -> carrinhoRepository.save(new Carrinho(pessoa)));
-     */
+      Carrinho carrinho = carrinhoRepository.findByPessoa(pessoa)
+     .orElseGet(() -> carrinhoRepository.save(new Carrinho(pessoa)));
+    */
     return "Carrinho criado!";
   }
 
   // metodo publico para adicionar jogo ao carrinho
   public void adicionarAoCarrinho(String cpf, Long Idjogo, int quantidade) {
-    // variavel do tipo pessoa armazenando objeto pessoa identificado com o cpf
-    // fornecido na assinatura do metodo
-    // caso a pessoa não seja encontra uma excessão será executada com a mensagem
-    // abaixo
+    /*
+     variavel do tipo pessoa armazenando objeto pessoa identificado com o cpf fornecido na assinatura do metodo
+     caso a pessoa não seja encontra uma excessão será executada com a mensagem abaixo
+    */
     Pessoa pessoa = pessoaRepository.findByCpf(cpf)
         .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
 
-    // variavel do tipo carrinho armazenando objeto carrinho associado a um objeto
-    // pessoa
-    // esse objeto pessoa é um atributo da classe carrinho e é recebido como
-    // argumento no construtor da classe
-    // o metodo findByPessoa é um optional criado para que a pessoa associada seja
-    // encontrada
-    // esse metodo personalizado só é possivel graças ao recebimento de pessoa no
-    // construtor da classe carrinho
-    // caso a pessoa seja encontrada ela é armazenada e caso contrario será criado
-    // um novo objeto de carrinho
+    /*
+     variavel do tipo carrinho armazenando objeto carrinho associado a um objeto pessoa
+     esse objeto pessoa é um atributo da classe carrinho e é recebido como argumento no construtor da classe (Carrinho)
+     o metodo findByPessoa é um optional criado (em CarrinhoRepository) para que a pessoa associada seja encontrada
+     esse metodo personalizado só é possivel graças ao recebimento de pessoa no construtor da classe carrinho
+     caso a pessoa seja encontrada ela é armazenada e caso contrario será criado um novo objeto de carrinho
+     */
     Carrinho carrinho = carrinhoRepository.findByPessoa(pessoa)
         .orElseGet(() -> carrinhoRepository.save(new Carrinho(pessoa)));
 
-    // variavel jogo criada para armazenar jogo encontrado por ID
-    // metodo findById ja existe na JPA, podemos utiliza-lo passando apenas um ID
-    // tipo long
-    // caso o jogo seja encontrado ele será armazenado e caso contrário uma exceção
-    // será lançada
+    /* 
+    variavel jogo criada para armazenar jogo encontrado por ID
+    metodo findById ja existe na JPA, podemos utiliza-lo passando apenas um ID tipo long
+    caso o jogo seja encontrado ele será armazenado e caso contrário uma exceção será lançada 
+    */ 
     Jogo jogo = jogoRepository.findById(Idjogo)
         .orElseThrow(() -> new RuntimeException("Jogo não encontrado!"));
 
     // variavel ItemCarrinho sendo criada inicialmente com valor null
     ItemCarrinho itemExistente = null;
-    // abaixo uma iteração sobre todos os itens presentes na lista de itens do
-    // carrinho em questão
-    // a lista de itens do carrinho é populada com objetos do tipo itemcarrinho e
-    // por isso deve
-    // ser iterada desta maneira
+    /* 
+    abaixo uma iteração sobre todos os itens presentes na lista de itens do carrinho em questão
+    a lista de itens do carrinho é populada com objetos do tipo itemcarrinho e por isso deve ser iterada desta maneira 
+    */
     for (ItemCarrinho item : carrinho.getItens()) {
-      // objetos do tipo item carrinho possuem atributos tipo jogo em suas classes
-      // caso o id recebido como argumento no metodo, a variavel itemexistente vai
-      // armazenar esse jogo
+      /*  objetos do tipo item carrinho possuem atributo tipo jogo em sua classe
+          caso o id recebido como argumento no metodo seja igual ao do jogo em questão, a variavel itemexistente vai armazenar esse jogo */
       if (item.getJogo().getId().equals(Idjogo)) {
         itemExistente = item;
         break;
