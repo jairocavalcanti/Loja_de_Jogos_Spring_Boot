15e3af427ccb066cc6b80612ebbc151913ad51d6
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2026-03-12 18:16:04 -0300

    Pequeno ajuste de identação

--- src/main/java/com/crudFrontend/crud/Security/SecurityConfig.java
+++ src/main/java/com/crudFrontend/crud/Security/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(authorize -> authorize
       .requestMatchers("/auth/**").permitAll()
-      .requestMatchers("/pessoa/**").permitAll().
-      requestMatchers("/carrinho/**").permitAll()
+      .requestMatchers("/pessoa/**").permitAll()
+      .requestMatchers("/carrinho/**").permitAll()
       .anyRequest().authenticated()
       
       ).addFilterBefore(securityfilter,UsernamePasswordAuthenticationFilter.class);
