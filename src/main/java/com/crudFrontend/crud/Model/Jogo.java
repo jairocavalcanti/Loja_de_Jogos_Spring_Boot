02273770ff247d212ade18528bbeb9a7b6983ed7
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2026-08-04 14:43:07 -0300

    Adicionando atributo gênero e anotações JsonProperty e Enumerated

--- src/main/java/com/crudFrontend/crud/Model/Jogo.java
+++ src/main/java/com/crudFrontend/crud/Model/Jogo.java
@@ -2,8 +2,13 @@ package com.crudFrontend.crud.Model;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.crudFrontend.crud.Enum.genero;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,13 +24,18 @@ public class Jogo {
     private String nome;
     
     @Column(nullable = false)
+    @JsonProperty("descricao")
     private String descricao;
 
     @Column()
+    @JsonProperty("urlimage")
     private String urlimage;
     
     @Column(nullable = false)
     private BigDecimal preco;
+
+    @Enumerated(EnumType.STRING) // Salva como STRING no banco (ex: "acao", "rpg")
+    private genero genero;
 
     public Long getId() {
         return id;
@@ -47,7 +57,7 @@ public class Jogo {
         return descricao;
     }
 
-    public void setDescrição(String descricao) {
+    public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
@@ -65,6 +75,14 @@ public class Jogo {
 
     public void setUrlimage(String urlimage) {
         this.urlimage = urlimage;
+    }
+
+    public genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(genero genero) {
+        this.genero = genero;
     }
 
 }
