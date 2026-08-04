a41ca74be947ad3f8473b1d5b4fa1509285b28e0
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2026-08-04 14:54:13 -0300

    Removendo anotação Repository redundante

--- src/main/java/com/crudFrontend/crud/Repository/CarrinhoRepository.java
+++ src/main/java/com/crudFrontend/crud/Repository/CarrinhoRepository.java
@@ -3,12 +3,9 @@ package com.crudFrontend.crud.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.crudFrontend.crud.Model.Carrinho;
 import com.crudFrontend.crud.Model.Pessoa;
 
-@Repository
 public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
     // Esse metodo busca por um carrinho que pertença a pessoa informada
     // Um carrinho cujo campo pessoa seja igual ao objeto Pessoa recebido como argumento
--- src/main/java/com/crudFrontend/crud/Repository/JogoRepository.java
+++ src/main/java/com/crudFrontend/crud/Repository/JogoRepository.java
@@ -3,11 +3,8 @@ package com.crudFrontend.crud.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.crudFrontend.crud.Model.Jogo;
 
-@Repository
 public interface JogoRepository extends JpaRepository<Jogo, Long> {
     Optional<Jogo> findByNome(String nome);
 }
